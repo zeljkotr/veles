e08402b15346ca46741c1cc2ad7c27040f78bd6c
Zeljko Tripcevski <TVOJ_GITHUB_EMAIL>
2026-08-31 07:18:34 -0400

    feat: add delivery pipeline step CRUD

--- veles/modules/delivery/service.py
+++ veles/modules/delivery/service.py
@@ -6,7 +6,7 @@ delivery sistema VELES.
 """
 
 from veles.database.connection import get_session
-from veles.database.models import Pipeline
+from veles.database.models import Pipeline, PipelineStep
 from veles.modules.infrastructure.resource_registry import ResourceRegistry
 
 
@@ -186,6 +186,208 @@ class DeliveryService:
                 return False
 
             session.delete(pipeline)
+            session.commit()
+
+            return True
+
+        except Exception:
+
+            session.rollback()
+            raise
+
+        finally:
+
+            session.close()
+
+    def add_pipeline_step(
+        self,
+        pipeline_id,
+        position,
+        name,
+        type,
+        configuration=None
+    ):
+
+        session = get_session()
+
+        try:
+
+            pipeline = (
+                session.query(Pipeline)
+                .filter(Pipeline.id == pipeline_id)
+                .first()
+            )
+
+            if pipeline is None:
+                return None
+
+            step = PipelineStep(
+                pipeline_id=pipeline_id,
+                position=position,
+                name=name,
+                type=type,
+                configuration=configuration or {}
+            )
+
+            session.add(step)
+            session.commit()
+            session.refresh(step)
+
+            return {
+                "id": step.id,
+                "pipeline_id": step.pipeline_id,
+                "position": step.position,
+                "name": step.name,
+                "type": step.type,
+                "configuration": step.configuration
+            }
+
+        except Exception:
+
+            session.rollback()
+            raise
+
+        finally:
+
+            session.close()
+
+    def get_pipeline_steps(self, pipeline_id):
+
+        session = get_session()
+
+        try:
+
+            pipeline = (
+                session.query(Pipeline)
+                .filter(Pipeline.id == pipeline_id)
+                .first()
+            )
+
+            if pipeline is None:
+                return None
+
+            steps = (
+                session.query(PipelineStep)
+                .filter(PipelineStep.pipeline_id == pipeline_id)
+                .order_by(PipelineStep.position, PipelineStep.id)
+                .all()
+            )
+
+            return [
+                {
+                    "id": step.id,
+                    "pipeline_id": step.pipeline_id,
+                    "position": step.position,
+                    "name": step.name,
+                    "type": step.type,
+                    "configuration": step.configuration
+                }
+                for step in steps
+            ]
+
+        finally:
+
+            session.close()
+
+    def get_pipeline_step(self, step_id):
+
+        session = get_session()
+
+        try:
+
+            step = (
+                session.query(PipelineStep)
+                .filter(PipelineStep.id == step_id)
+                .first()
+            )
+
+            if step is None:
+                return None
+
+            return {
+                "id": step.id,
+                "pipeline_id": step.pipeline_id,
+                "position": step.position,
+                "name": step.name,
+                "type": step.type,
+                "configuration": step.configuration
+            }
+
+        finally:
+
+            session.close()
+
+    def update_pipeline_step(
+        self,
+        step_id,
+        position=None,
+        name=None,
+        type=None,
+        configuration=None
+    ):
+
+        session = get_session()
+
+        try:
+
+            step = (
+                session.query(PipelineStep)
+                .filter(PipelineStep.id == step_id)
+                .first()
+            )
+
+            if step is None:
+                return None
+
+            if position is not None:
+                step.position = position
+
+            if name is not None:
+                step.name = name
+
+            if type is not None:
+                step.type = type
+
+            if configuration is not None:
+                step.configuration = configuration
+
+            session.commit()
+            session.refresh(step)
+
+            return {
+                "id": step.id,
+                "pipeline_id": step.pipeline_id,
+                "position": step.position,
+                "name": step.name,
+                "type": step.type,
+                "configuration": step.configuration
+            }
+
+        except Exception:
+
+            session.rollback()
+            raise
+
+        finally:
+
+            session.close()
+
+    def delete_pipeline_step(self, step_id):
+
+        session = get_session()
+
+        try:
+
+            step = (
+                session.query(PipelineStep)
+                .filter(PipelineStep.id == step_id)
+                .first()
+            )
+
+            if step is None:
+                return False
+
+            session.delete(step)
             session.commit()
 
             return True
